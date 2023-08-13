3b283403cf6020d9c5228a019d6be40080d94a82
Dhruvi <dhruvipatel282@gmail.com>
2023-08-13 02:29:36 -0400

    Tests are a critical component of software quality control
    * How to make assertions in local and instrumentation tests.
    * How to use test rules.

--- app/src/main/java/dhruvi/patel/tipcalculatorjetpackcompose/MainActivity.kt
+++ app/src/main/java/dhruvi/patel/tipcalculatorjetpackcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -60,8 +61,8 @@ class MainActivity : ComponentActivity() {
     }
 }//
 
-
-private fun calculateTip(
+@VisibleForTesting
+internal fun calculateTip(
     amount: Double,
     tipPercent : Double = 15.0,
     roundUp: Boolean
